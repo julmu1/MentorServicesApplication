b3e578fadd6c58059058d5fb2f1158574be96377
nhsbsa-julmu <julmu@nhsbsa.nhs.uk>
2025-01-03 12:18:45 +0000

    Fix Nine: adding debugging step from requirements.txt

--- mentor_apprentice_services/settings.py
+++ mentor_apprentice_services/settings.py
@@ -132,6 +132,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
